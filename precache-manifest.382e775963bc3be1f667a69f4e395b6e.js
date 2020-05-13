da7ed1b2186d6c39d1b6f920579fb830f15991a6
Владимир <vladimir@iMac-Aleksandr.local>
2020-05-13 16:47:23 +0300

    Updates

---
+++ precache-manifest.382e775963bc3be1f667a69f4e395b6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff47a031f185bba68a15f134913124eb",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "add5f75e475dd0a2db0c",
+    "url": "/forum/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "5ab8d70c0be1b16a0913",
+    "url": "/forum/static/css/main.9e5a0715.chunk.css"
+  },
+  {
+    "revision": "add5f75e475dd0a2db0c",
+    "url": "/forum/static/js/2.5cd14a61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/forum/static/js/2.5cd14a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab8d70c0be1b16a0913",
+    "url": "/forum/static/js/main.069086e3.chunk.js"
+  },
+  {
+    "revision": "b5486dd6e2168efe0606",
+    "url": "/forum/static/js/runtime-main.8bc28bf6.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/forum/static/media/logo.0c089234.svg"
+  }
+]);
