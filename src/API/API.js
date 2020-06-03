593e1f918fa6a630666a1a349206154f0a5687f1
feniks916 <53240482+feniks916@users.noreply.github.com>
2020-06-03 16:35:08 +0300

    done

--- src/API/API.js
+++ src/API/API.js
@@ -1,8 +1,30 @@
 import * as axios from 'axios';
+import { getJwt } from '../helpers/token';
 
 const instance = axios.create({
   baseURL: 'https://conduit.productionready.io',
 });
+
+instance.interceptors.request.use(config => {
+  console.log(config)
+  const jwt = getJwt()
+  if (jwt) {
+  config.headers['Authorization'] = jwt
+  console.log('hello')
+  }
+  return config
+  }, err => {
+    console.log('mistake')
+    return Promise.reject(err)
+  })
+
+  axios.interceptors.response.use(function (response) {
+    console.log(response)
+    return response;
+  }, function (error) {
+    console.log(error)
+    return Promise.reject(error);
+  });
 
 export const register = registerData =>
 instance.post('/api/users', registerData)
